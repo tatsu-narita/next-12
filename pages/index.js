177e3f5013621ce661db50247042f84b718096e9
{tatsu-narita} <{nt0604.b@gmail.com}>
2022-10-09 23:59:59 +0900

    COMMIT

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className="hero">
       <h1>CUBE</h1>
       <p>アウトプットしていくサイト</p>
     </div>
