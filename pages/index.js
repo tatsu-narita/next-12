32da6fd279b0c26457570bd15c6bf70f5f32ad41
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-10-12 23:59:59 +0900

    COMMIT

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className="hero">
-      <h1>CUBE</h1>
+      <h1 style={{ color: 'red', fontSize: '88px' }}>
+        CUBE
+      </h1>
       <p>アウトプットしていくサイト</p>
     </div>
   )
