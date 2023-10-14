4b0f65d350ad00e45362a77e906707bedffd023e
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-10-14 23:59:18 +0900

    update: header
    tuika

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,8 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="hero">
-      <h1 style={{ color: 'red', fontSize: '88px' }}>
-        CUBE
-      </h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
+    <>
+    <header></header>
+    </>
   )
 }
