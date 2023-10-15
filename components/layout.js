a4d1e6bf471a0267ff652440823fed95e474f8a0
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-10-15 21:11:03 +0900

    add: layout.jsの作成
    Header/Footerをlayoutコンポーネントで管理

---
+++ components/layout.js
@@ -0,0 +1,10 @@
+import Layout from 'components/layout'
+import Hero from 'components/hero'
+
+export default function Layout({ children }) {
+    return (
+        <Layout>
+            <Hero />
+        </Layout>
+    )
+}
