bd15b9a43a11b60c8e5240dde516cb3df07e691a
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-10-27 23:58:20 +0900

    update: ヒーロー画像
    画像表示

--- components/hero.js
+++ components/hero.js
@@ -1,4 +1,6 @@
 import styles from "styles/hero.module.css"
+import Image from "next/image"
+import cube from "images/cube.jpg"
 
 export default function Hero({ title, subtitle, imageOn = false}) {
     return (
@@ -7,7 +9,11 @@ export default function Hero({ title, subtitle, imageOn = false}) {
                 <h1 className={styles.title}>{title}</h1>
                 <p className={styles.subtitle}>{subtitle}</p>
             </div>
-            {imageOn && <figure> [画像] </figure>}
+            {imageOn && (
+            <figure>
+                <Image src={cube} alt="" layout="responsive" />
+            </figure>
+            )}
         </div>
     )
 }
