e683f980f0576ec526a77d74c786a62aad5b1918
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-10-31 23:50:51 +0900

    update: 修正
    その他修正

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,15 @@
 import Container from "components/container"
 import Logo from "components/logo"
-
+import Social from "components/social"
 import styles from "styles/footer.module.css"
 
 export default function Footer() {
     return (
         <footer className={styles.wrapper}>
-            <Container>
+            <Container >
                 <div className={styles.flexContainer}>
                     <Logo />
-                    
+                    <Social />
                 </div>
             </Container>
         </footer>
