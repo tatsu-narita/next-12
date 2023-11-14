bf6ff7744f886f94a02b81dc398e7658a4d039d7
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-11-15 06:49:07 +0900

    add: eyecatch代替
    ローカルの代替画像を用意

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -9,6 +9,9 @@ import ConvertBody from "components/convert-body"
 import PostCategories from "components/post-categories"
 import Image from "next/image"
 
+//ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from "lib/constants"
+
 export default function Schedule({
     title,
     publish,
@@ -57,18 +60,20 @@ export default function Schedule({
 }
 
 export async function getStaticProps() {
-    const slug = "schedule"
+    const slug = "micro"
 
     const post = await getPostBySlug(slug)
 
     const description = extractText(post.content)
+
+    const eyecatch = post.eyecatch ?? eyecatchLocal
 
     return {
         props: {
             title: post.title,
             publish: post.publishDate,
             content: post.content,
-            eyecatch: post.eyecatch,
+            eyecatch: eyecatch,
             categories: post.categories,
             description: post.description,
         },
