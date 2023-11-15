a3bad5a2efaeadd124fb8a6899b400849e2c3bc9
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-11-16 06:43:56 +0900

    update: Dynamic Routes
    記事ページを生成

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -13,7 +13,7 @@ import { getPlaiceholder } from "plaiceholder"
 //ローカルの代替アイキャッチ画像
 import { eyecatchLocal } from "lib/constants"
 
-export default function Schedule({
+export default function Post({
     title,
     publish,
     content,
@@ -62,8 +62,15 @@ export default function Schedule({
     )
 }
 
-export async function getStaticProps() {
-    const slug = "micro"
+export async function getStaticPaths() {
+    return {
+        paths: ["/blog/schedule", "/blog/music", "/blog/micro"],
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const slug = context.params.slug
 
     const post = await getPostBySlug(slug)
 
