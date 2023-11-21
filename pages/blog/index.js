174cd5aedeef267ccfd063e1aba84b8623cb8bfa
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-11-22 07:22:05 +0900

    update: 代替画像とブラー画像追加
    for..of使用

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,7 +3,10 @@ import Meta from "components/meta"
 import Container from "components/container"
 import Hero from "components/hero"
 import Posts from "components/posts"
+import { getPlaiceholder } from "plaiceholder"
 
+//ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from "lib/constants"
 
 export default function Blog({ posts }) {
     return (
@@ -21,6 +24,14 @@ export default function Blog({ posts }) {
 
 export async function getStaticProps() {
     const posts = await getAllPosts()
+
+    for (const post of posts) {
+        if (!post.hasOwnProperty("eyecatch")) {
+            post.eyecatch = eyecatchLocal
+        }
+        const { base64 } = await getPlaiceholder(post.eyecatch.url)
+        post.eyecatch.blurDateURL = base64
+    }
 
     return {
         props: {
