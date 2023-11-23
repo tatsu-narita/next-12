ad461f19f0f02b5fb61304ccdd8d19f657dbba69
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-11-23 23:01:08 +0900

    update: 記事一覧
    レイアウトを整える

--- components/posts.js
+++ components/posts.js
@@ -16,6 +16,7 @@ export default function Posts({ posts }) {
                                     layout="responsive"
                                     width={eyecatch.width}
                                     height={eyecatch.height}
+                                    sizes="(min-width: 1152px) 576px, 50vw"
                                     placeholder="blur"
                                     blurDataURL={eyecatch.blurDataURL}
                                 />
