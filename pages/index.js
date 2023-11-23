9608917a28a7039ce7d2757302cea5a45691d7eb
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-11-24 07:06:16 +0900

    update: トップページ
    記事一覧を表示

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import { getAllPost } from "lib/api"
+
 import Container from "components/container"
 import Hero from "components/hero"
 import Meta from "components/meta"
 
-export default function Home() {
+import Posts from "components/posts"
+import pagination from "components/pagination"
+import { getPlaiceholder } from "plaiceholder"
+
+//ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from "lib/constants"
+
+export default function Home({posts}) {
   return (
     <Container>
       <Meta />
@@ -11,6 +20,26 @@ export default function Home() {
     subtitle="アウトプットしていくサイト"
     imageOn
     />
+    <Posts posts={posts} />
+    <pagination nextUrl="/blog" nextText="More Posts" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty("eyecatch")) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDateURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
