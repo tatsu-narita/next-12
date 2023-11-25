9f954d0f3f721817750aa17248258312f3e39dba
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-11-25 22:47:24 +0900

    add: 指定したスラッグのカテゴリーページを生成
    blog/category/[slug].js

---
+++ pages/blog/category/[slug].js
@@ -0,0 +1,31 @@
+import { getAllCategories } from "lib/api";
+import Container from "components/container";
+import PostHeader from "components/post-header";
+
+export default function Category({ name }) {
+    return (
+        <Container>
+            <PostHeader title={name} subtitle="Blog Category" />
+        </Container>
+    )
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: ["/blog/category/technology"],
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const catSlug = context.params.slug
+
+    const allCats = await getAllCategories()
+    const cat = allCats.find(({ slug }) => slug === catSlug)
+
+    return {
+        props: {
+            name: cat,name,
+        },
+    }
+}
