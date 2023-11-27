528ed32aa1191ad8aaf5375625c1bfec57c943ee
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-11-28 07:13:35 +0900

    update: ハンバーガーメニューの作成
    メニュー開閉の状態を管理するstateを用意

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,11 @@
+import { useState } from "react"
 import Link from "next/link"
 import styles from "styles/nav.module.css"
 
 export default function Nav() {
+    const [navIsOpen, setNavIsOpen] = useState(false)
     return (
-        <nav>
+        <nav className={navIsOpen ? styles.open : styles.close}>
             <btoa className={styles.btn}>MENU</btoa>
 
             <ul className={styles.list}>
