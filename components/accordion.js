12ccf9f7602fe0bf3547d909d0fb1880ce2c5193
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-12-02 10:39:35 +0900

    update: アコーディオン作成
    Accordionコンポーネント

---
+++ components/accordion.js
@@ -0,0 +1,19 @@
+import styles from "styles/accordion.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCircleChevronDown } from "@fortawesome/free-solid-svg-icons"
+
+export default function Accordion({ heading, children }) {
+    return (
+        <div className={styles.open}>
+            <h3 className={styles.heading}>
+                <button>
+                    {heading}
+                    <FontAwesomeIcon icon={faCircleChevronDown} className={styles.icon} />
+                </button>
+            </h3>
+            <div className={styles.text}>
+                <div className={styles.textInner}>{children}</div>
+            </div>
+        </div>
+    )
+}
