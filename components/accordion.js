9bc12eb6645f8118c01ca0c7924c083661bd1c5e
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-12-03 21:21:13 +0900

    update: アコーディオンの作成
    開閉の状態を管理２

--- components/accordion.js
+++ components/accordion.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleChevronDown } from "@fortawesome/free-solid-svg-icons"
 
 export default function Accordion({ heading, children }) {
+    const [textIsOpen, setTextOpen] = useState(false)
     return (
         <div className={styles.open}>
             <h3 className={styles.heading}>
