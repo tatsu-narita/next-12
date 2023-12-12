a8ec4dce9c37e3e4c5a3b1484f00b9c7894485ca
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-12-13 07:16:20 +0900

    add: サイトマップ作成
    動的サイトマップの作成

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,10 @@
 /** @type {import ("next-sitemap").IConfig} */
 const config = {
     siteUrl: "",
-    outDir: "./out",
+    exclude: ["/server-sitemap.xml"],
+    robotsTxtOptions: {
+        additionalSitemaps: ["https://*****/server-sitemap.xml"],
+    },
 }
 
 module.exports = config
