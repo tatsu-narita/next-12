e10a31bcd67f831bbdcc53cd0752d642582c783c
{tatsu-narita} <{nt0604.b@gmail.com}>
2023-12-18 07:15:35 +0900

    update: 静的サイトジェネレーターSSG
    修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import ("next-sitemap").IConfig} */
 const config = {
     siteUrl: "",
+    outDir: "./out",
     exclude: ["/server-sitemap.xml"],
     robotsTxtOptions: {
         additionalSitemaps: ["https://*****/server-sitemap.xml"],
