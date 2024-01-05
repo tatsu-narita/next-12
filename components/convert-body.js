bdd4668e061a86b208d89b34b1fb9ecfe47e2abf
{tatsu-narita} <{nt0604.b@gmail.com}>
2024-01-05 19:06:10 +0900

    update: 変換処理を行うコンポーネント
    修正

--- components/convert-body.js
+++ components/convert-body.js
@@ -2,7 +2,7 @@ import parse from "html-react-parser"
 import Image from "next/image"
 
 export default function ConvertBody({ contentHTML }) {
-    const contentReact = paser(contentHTML, {
+    const contentReact = parse(contentHTML, {
         replace: (node) => {
             if (node.name === "img") {
                 const { src, alt, width, height } = node.attribs
