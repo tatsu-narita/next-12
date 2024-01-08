402b494e3c7264126c8fe9696d504dd0b97db2fb
{tatsu-narita} <{nt0604.b@gmail.com}>
2024-01-09 06:45:00 +0900

    update: 優先読み込みとブラー
    修正

--- components/hero.js
+++ components/hero.js
@@ -17,16 +17,16 @@ export default function Hero({ title, subtitle, imageOn = false}) {
                 <p className={styles.subtitle}>{subtitle}</p>
             </div>
             {imageOn && (
-            <figure className={styles.image}>
-                <Image
-                    src={cube}
-                    alt=""
-                    layout="responsive"
-                    sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
-                    priority
-                    placeholder="blur"
-                />
-            </figure>
+                <figure className={styles.image}>
+                    <Image
+                        src={cube}
+                        alt=""
+                        layout="responsive"
+                        sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
+                        priority
+                        placeholder="blur"
+                    />
+                </figure>
             )}
         </div>
     )
