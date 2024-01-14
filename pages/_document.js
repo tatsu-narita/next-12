73c4b11a69c4ffb4d7759a6965484be3bd7718a5
{tatsu-narita} <{nt0604.b@gmail.com}>
2024-01-14 18:05:14 +0900

    update: webフォントの最適化
    googl fontsで取得したwebフォントの設定

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,12 @@ const { siteLang } = siteMeta
 export default function Document() {
     return (
         <Html lang={siteLang}>
-            <Head />
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
